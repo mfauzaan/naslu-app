dd390ddbd4abc67e613895d9673f9a191e6a0a31
Fauzaan <fauzaan@socar.my>
2021-10-25 18:13:42 +0500

    Disabled island creation

--- pages/admin/islands/index.tsx
+++ pages/admin/islands/index.tsx
@@ -83,7 +83,7 @@ export default function IslandList() {
               />
             </div>
               <button
-                onClick={() => setOpen(true)}
+                disabled
                 type="button"
                 className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
