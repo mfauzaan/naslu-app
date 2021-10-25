3984215d73ebd3e1d5a0ab3b6506f5bb44dd841d
Fauzaan <fauzaan@socar.my>
2021-10-25 18:11:42 +0500

    Added admin

---
+++ pages/admin/index.tsx
@@ -0,0 +1,72 @@
+import Head from 'next/head'
+import React, { ReactElement, useEffect, useRef } from 'react'
+import Layout from '../../components/layout';
+import NestedLayout from '../../components/nested-layout';
+
+function GradientText({ text, from, to, style = {} }) {
+  return (
+    <span
+      className={`cursor-hand pl-3 relative bg-clip-text bg-gradient-to-r ${from} ${to}`}
+      style={style}
+    >
+      {text}
+    </span>
+  );
+}
+
+export default function AdminIndex() {
+  const dspRef = useRef();
+
+  useEffect(() => {
+    const dspContainer: any = dspRef.current;
+    
+    const tween = {
+      css: {
+        color: "transparent",
+      },
+      repeat: 1,
+      yoyo: true,
+    };
+
+    // Use dynamic import for gsap to make sure that it's loaded in client side
+    (async () => {
+      const { TimelineMax } = await import('gsap');
+
+      const tl = new TimelineMax({ repeat: -1 });
+      for (let i = 0; i < dspContainer.children.length; i++) {
+        tl.to(dspContainer.children[i], 1.2, tween);
+      }
+    })();
+  }, []);
+  
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen -my-28">
+      <Head>
+        <title>Naslu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div
+        className="mb-12 flex flex-col items-center justify-center font-extrabold tracking-tighter text-6xl md:text-7xl"
+        style={{ lineHeight: 1.2 }}
+      >
+      </div>
+      <main className="flex flex-col md:flex-row items-center justify-center md:px-20 text-center text-6xl font-bold">
+        <h1>
+          Welcome to{' '}
+        </h1>
+        <div ref={dspRef}>
+          <GradientText text="Naslu Admin" from="from-indigo-600" to="to-green-400"/>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+AdminIndex.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      <NestedLayout>{page}</NestedLayout>
+    </Layout>
+  );
+};
