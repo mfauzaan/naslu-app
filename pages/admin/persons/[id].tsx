5d0306866294c8f0ca2d800f85c54445998cc222
Fauzaan <fauzaan@socar.my>
2021-10-31 15:53:35 +0500

    Added address to show page

--- pages/admin/persons/[id].tsx
+++ pages/admin/persons/[id].tsx
@@ -86,9 +86,13 @@ export default function PersonShow() {
               </div>
               <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
                 <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-                  <div className="sm:col-span-2">
+                  <div className="sm:col-span-1">
                     <dt className="text-sm font-medium text-gray-500">ID card number</dt>
                     <dd className="mt-1 text-sm text-gray-900">{person.idCardNumber}</dd>
+                  </div>
+                  <div className="sm:col-span-1">
+                    <dt className="text-sm font-medium text-gray-500">Address</dt>
+                    <dd className="mt-1 text-sm text-gray-900">{person.address.address}, <span className="mt-1 text-sm text-gray-500">{person.address.island.atoll}. {person.address.island.name}</span></dd>
                   </div>
                   <div className="sm:col-span-1">
                     <dt className="text-sm font-medium text-gray-500">First name</dt>
